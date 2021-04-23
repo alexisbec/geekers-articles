f59555c1af1ffa0b1b6270358d095b518383150c
Alexis Varela <alexisbec13@hotmail.com>
2021-04-22 20:30:11 -0500

    add styles to navbar and add logo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,10 +26,8 @@ class ArticlesController < ApplicationController
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show, :index]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :categories, only: [:show]
+  resources :categories, only: [:show, :index]
   resources :articles do
     post '/vote/', to: 'votes#create'
     delete '/vote/', to: 'votes#destroy'
