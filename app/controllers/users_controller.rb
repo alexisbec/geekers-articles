ba7314459aefaac113d834ba1909b807de0810b3
Alexis Varela <alexisbec13@hotmail.com>
2021-05-12 11:22:21 -0500

    add foreign keys to article and vote model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,13 +21,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        session[:user_id] = @user.id
-        format.html { redirect_to root_path, notice: 'User was successfully created.' }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'User was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,8 +2,8 @@ class Vote < ApplicationRecord
   validates :user_id, presence: true
   validates :article_id, uniqueness: { scope: :user_id }
 
-  belongs_to :user, dependent: :destroy
-  belongs_to :article, dependent: :destroy
+  belongs_to :user, foreign_key: 'user_id', dependent: :destroy
+  belongs_to :article, foreign_key: 'article_id', dependent: :destroy
 
   def self.most_voted
     total_votes = Vote.group(:article_id).count
